0df593286781736d883d6ab59d57ecfd2f2849fe
robmllze <86869208+robmllze@users.noreply.github.com>
2024-08-05 22:42:50 +1000

    chore: Update workflow scripts

--- lib/src/celestial_radius.dart
+++ lib/src/celestial_radius.dart
@@ -1,8 +1,8 @@
 //.title
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //
-// Dart/Flutter (DF) Packages by DevCetra.com & contributors. See MIT LICENSE
-// file in the root directory.
+// Dart/Flutter (DF) Packages by DevCetra.com & contributors. See LICENSE file
+// in root directory.
 //
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 //.title~
@@ -42,8 +42,7 @@ enum CelestialRadius {
   double get inNauticalMiles => this.convert(DistanceUnit.NAUTICAL_MILES);
   double get inFeet => this.convert(DistanceUnit.FEET);
   double get inYards => this.convert(DistanceUnit.YARDS);
-  double get inAstronomicalUnits =>
-      this.convert(DistanceUnit.ASTRONOMICAL_UNITS);
+  double get inAstronomicalUnits => this.convert(DistanceUnit.ASTRONOMICAL_UNITS);
 
   //
   //
